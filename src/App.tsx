6378d5a184d03be336cbc49e738a1f8c5f5b55e1
junaid-hossain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-03-08 10:05:59 +0600

    feat: add RandomColorGenerator component and update App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Accordion from "./components/Accordion"
+import RandomColorGenerator from "./components/RandomColorGenerator"
 
 
 function App() {
  
 
   return (
-    <div className="flex justify-center items-center h-screen m-4 p-4">
+    <div className="flex flex-col justify-center items-center h-screen m-4 p-4">
       <Accordion />
+      <RandomColorGenerator />
     </div>
   )
 }
---
+++ src/components/RandomColorGenerator.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+
+export default function RandomColorGenerator() {
+
+  function generateRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      // eslint-disable-next-line react-hooks/purity
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    console.log(color);
+    return color;
+  }
+
+  const [color, setColor] = React.useState<string>(generateRandomColor());
+  const randomColor = color;
+
+  return (
+    <div className="flex flex-col text-center justify-center w-2xl m-4 p-4">
+        <style>
+          {`
+            body{
+                 background-color: ${randomColor};
+            }
+          `}
+        </style>
+      <h1 className="font-mono font-extrabold text-3xl mb-3">Random Color Generator</h1>
+      <button onClick={() => setColor(generateRandomColor())} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Generate Color
+      </button>
+    </div>
+  );
+}
